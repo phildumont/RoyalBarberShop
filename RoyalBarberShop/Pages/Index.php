6e709eeb21549c3132fa68fd5eef5b0547a1a9e7
phildumont <32916109+phildumont@users.noreply.github.com>
2017-12-11 16:14:38 -0500

    Fixed carousel

--- RoyalBarberShop/Pages/Index.php
+++ RoyalBarberShop/Pages/Index.php
@@ -38,6 +38,42 @@
 	</nav>
 	<!-- Nav bar end -->
 	<h1 class="myTitle text-center">Royal Barber Shop</h1>
+	
+	
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+			<img src="../Content/Images/Index/_MG_3100.jpg" alt="Image1" class="carousel_picture img-responsive center-block">
+		</div>
+
+		<div class="item">
+			<img src="../Content/Images/Index/_MG_3497.jpg" alt="Image2" class="carousel_picture img-responsive center-block">
+		</div>
+
+		<div class="item">
+			<img src="../Content/Images/Index/_MG_3532.jpg" alt="Image3" class="carousel_picture img-responsive center-block">
+		</div>
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	
 	<div class="container-fluid">
 		<div class="jumbotron">
 			<div class="row">
@@ -53,39 +89,9 @@
 		</div>
 	</div>
 	
-	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-			<li data-target="#myCarousel" data-slide-to="2"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner">
-			<div class="item active">
-			<img src="../Content/Images/Index/_MG_3111.jpg" alt="Image1" class="">
-		</div>
-
-		<div class="item">
-			<img src="../Content/Images/Index/_MG_3497.jpg" alt="Image2" class="">
-		</div>
-
-		<div class="item">
-			<img src="../Content/Images/Index/_MG_3532.jpg" alt="Image3" class="">
-		</div>
-		</div>
-
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#myCarousel" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
+	<footer>
+		<!-- TODO put text in footer -->
+	</footer>
 </body>
 </html>
 
