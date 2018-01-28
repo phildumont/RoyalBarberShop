c1b97dcc45a1205ef4ed57e4501687ad1d46bbf9
phildumont <32916109+phildumont@users.noreply.github.com>
2018-01-28 16:25:14 -0500

    fixed carousel on index.php

--- RoyalBarberShop/Pages/Index.php
+++ RoyalBarberShop/Pages/Index.php
@@ -14,7 +14,6 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="Stylesheet" type="text/css" href="../Content/Stylesheets/bootstrap.min.css">
 	<link rel="Stylesheet" type="text/css" href="../Content/Stylesheets/mainStylesheet.css">
-	<link rel="stylesheet" type="text/css" href="../Content/Stylesheets/style.css" />
 	<script type="text/javascript" src="../Content/Scripts/jquery.js"></script>
 </head>
 <body class="mainBackground">
@@ -27,19 +26,38 @@
 	<!-- Nav bar end -->
 	<h1 class="myTitle text-center">Royal Barber Shop</h1>
 	
-	<!--  Slideshow start -->
-	<div id="wowslider-container1">
-		<div class="ws_images"><ul>
-			<li><img src="../Content/Images/Index/_MG_3100.jpg" alt="image1" id="wows1_0"/></li>
-			<li><img src="../Content/Images/Index/_MG_3497.jpg" alt="image2" id="wows1_1"/></li>
-			<li><img src="../Content/Images/Index/_MG_3532.jpg"  alt="image3" id="wows1_2"/></li>
-		</ul></div>
-		<div class="ws_script" style="position:absolute;left:-99%"></div>
-		<div class="ws_shadow"></div>
+	<!--  Carousel start -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner carousel_img">
+			<div class="item active">
+				<img src="../Content/Images/Index/_MG_3100.jpg" alt="image1"/>				
+			</div>
+			<div class="item">
+				<img src="../Content/Images/Index/_MG_3497.jpg" alt="image2"/>
+			</div>
+			<div class="item">
+				<img src="../Content/Images/Index/_MG_3532.jpg"  alt="image3"/>
+			</div>
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		</div>
 	</div>
-	<script type="text/javascript" src="../Content/Scripts/wowslider.js"></script>
-	<script type="text/javascript" src="../Content/Scripts/script.js"></script>
-	<!-- Slideshow end -->
+	<!-- Carousel end -->
 	
 	<!--About section start -->
 	<div class="container-fluid" style="padding-left:0px;padding-right:0px;">
